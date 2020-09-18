2314cdfdde1cf1348f5f555c98a254f30f7f533b
Hector Rodriguez <phreaker_boy@hotmail.com>
2020-09-18 16:47:55 -0700

    Add some constants for mouse move

--- BLEGuitar.Server/DXHelper.cs
+++ BLEGuitar.Server/DXHelper.cs
@@ -20,6 +20,30 @@ namespace BLEGuitar.Server
             ScanCode = 0x0008
         }
 
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-mouseinput
+        /// </summary>
+        [Flags]
+        private enum MouseFlag
+        {
+            MOUSEEVENTF_ABSOLUTE = 0x8000,
+            MOUSEEVENTF_HWHEEL = 0x01000,
+            MOUSEEVENTF_MOVE = 0x0001,
+            MOUSEEVENTF_MOVE_NOCOALESCE = 0x2000,
+            MOUSEEVENTF_LEFTDOWN = 0x0002,
+            MOUSEEVENTF_LEFTUP = 0x0004
+        }
+
+        /// <summary>
+        /// https://docs.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-input
+        /// </summary>
+        private enum InputType : int
+        {
+            INPUT_MOUSE = 0,
+            INPUT_KEYBOARD = 1,
+            INPUT_HARDWARE = 2
+        }
+
         [DllImport("user32.dll")]
         static extern UInt32 SendInput(UInt32 nInputs, [MarshalAs(UnmanagedType.LPArray, SizeConst = 1)] INPUT[] pInputs, Int32 cbSize);
 
@@ -79,12 +103,24 @@ namespace BLEGuitar.Server
         {
             INPUT[] InputData = new INPUT[1];
 
-            InputData[0].type = 1;
+            InputData[0].type = (int)InputType.INPUT_KEYBOARD;
             InputData[0].ki.wScan = keycode;
             InputData[0].ki.dwFlags = (int)(flag | KeyFlag.ScanCode);
             InputData[0].ki.time = 0;
             InputData[0].ki.dwExtraInfo = IntPtr.Zero;
 
+            SendInput(1, InputData, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+        private void SendMouseInput(int x, int y)
+        {
+            INPUT[] InputData = new INPUT[1];
+            InputData[0].type = (int)InputType.INPUT_MOUSE;
+            InputData[0].mi.mouseData = 0;
+            InputData[0].mi.time = 0;
+            InputData[0].mi.dx = x;
+            InputData[0].mi.dy = y;
+            InputData[0].mi.dwFlags = (int)MouseFlag.MOUSEEVENTF_MOVE;
             SendInput(1, InputData, Marshal.SizeOf(typeof(INPUT)));
         }
     }
